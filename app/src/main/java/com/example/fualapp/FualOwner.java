e0db294253a2ce1218043b142e3e69a99e2ba2a1
it19062884Ruvindu <it19062884@my.sliit.lk>
2022-10-22 19:14:22 +0530

    User and Station Owner Home Pages Added

--- app/src/main/java/com/example/fualapp/FualOwner.java
+++ app/src/main/java/com/example/fualapp/FualOwner.java
@@ -3,15 +3,15 @@ package com.example.fualapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FualOwner extends AppCompatActivity {
 
-    private TextView title;
+    private TextView title,Account;
     private EditText sb,msg;
     String  messagerk ="";
 
@@ -21,21 +21,37 @@ public class FualOwner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fual_owner);
 
-        title = (TextView) findViewById(R.id.Welcometae);
+        title = (TextView) findViewById(R.id.textView1);
+        Account = (TextView) findViewById(R.id.textView2);
 
         Intent intent = getIntent();
         messagerk = intent.getStringExtra(Authentication.EXTRA_MESSAGE);
 
         title.setText("Welcome "+messagerk+" !");
+        Account.setText(""+messagerk+" Authorized");
+
     }
 
     public void sendToInitialLanding(View view) {
         Intent intent = new Intent(this,  FirstNavigation.class);
-        ImageButton image = (ImageButton) findViewById(R.id.back);
+        Button image = (Button) findViewById(R.id.textView4);
         startActivity(intent);
     }
 
     // Implimet Methordes From Here :-
 
+    //Sending to Including Station :--
+    public void sendToRegisterStation(View view) {
+        Intent intent = new Intent(this,  MainActivity.class); // Rename with correct page
+        Button image = (Button) findViewById(R.id.button);
+        startActivity(intent);
+    }
+
+
+    public void sendToUpdateStationStatus(View view) {
+        Intent intent = new Intent(this,  MainActivity.class); // Rename with correct page
+        Button image = (Button) findViewById(R.id.button2);
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/com/example/fualapp/FualUser.java
+++ app/src/main/java/com/example/fualapp/FualUser.java
@@ -3,14 +3,14 @@ package com.example.fualapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FualUser extends AppCompatActivity {
 
-    private TextView title;
+    private TextView title,Account;
     String  messagerk ="";
 
     @Override
@@ -18,22 +18,42 @@ public class FualUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fual_user);
 
-        title = (TextView) findViewById(R.id.stwe);
+        title = (TextView) findViewById(R.id.textView1);
+        Account = (TextView) findViewById(R.id.textView2);
 
         Intent intent = getIntent();
         messagerk = intent.getStringExtra(Authentication.EXTRA_MESSAGE);
 
         title.setText("Welcome "+messagerk+" !");
-
+        Account.setText(""+messagerk+" Authorized");
     }
 
     public void sendToInitialLanding(View view) {
         Intent intent = new Intent(this,  FirstNavigation.class);
-        ImageButton image = (ImageButton) findViewById(R.id.back);
+        Button image = (Button) findViewById(R.id.textView4);
         startActivity(intent);
     }
 
     // Implimet Methordes From Here :-
+
+    public void sendToAddToQ(View view) {
+        Intent intent = new Intent(this,  MainActivity.class); // Rename with correct page
+        Button image = (Button) findViewById(R.id.button);
+        startActivity(intent);
+    }
+
+    public void sendToViewmyVehicleStatus(View view) {
+        Intent intent = new Intent(this,  MainActivity.class); // Rename with correct page
+        Button image = (Button) findViewById(R.id.button2);
+        startActivity(intent);
+    }
+
+    public void sendToViewStationStatus(View view) {
+        Intent intent = new Intent(this,  MainActivity.class); // Rename with correct page
+        Button image = (Button) findViewById(R.id.button3);
+        startActivity(intent);
+    }
+
 
 }
 

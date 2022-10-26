e8454582ae40dc8f8541ab6482d36606c596f6a8
Praveena Thavarajah <praveena.thavarajah@bizentra.com>
2022-10-26 18:01:19 +0530

    Code Cleaned Code Checked One

--- app/src/main/java/com/example/fualapp/MainActivity.java
+++ app/src/main/java/com/example/fualapp/MainActivity.java
@@ -20,7 +20,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button1,button2;
     public static String BASE_URL2 = "http://10.0.2.2:3000";
     private Retrofit retrofit;
     private RetrofitInterface retrofitInterface;
@@ -35,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     /***  ------------------------------------------------------------------------------------------------------  ***/
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,161 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         retrofitInterface = retrofit.create(RetrofitInterface.class);
 
-
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, StationOwner.class);
-                startActivity(intent);
-            }
-        });
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Retrievestationdetails.class);
-                startActivity(intent);
-            }
-        });
-
-
-        //01
-        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleLoginDialog();
-            }
-        });
-
-        //02
-        findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleSignupDialog();
-            }
-        });
-
-
-
     }
-
-
-
-    private void handleLoginDialog() {
-
-        View view = getLayoutInflater().inflate(R.layout.login_dialog, null);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setView(view).show();
-
-        Button loginBtn = view.findViewById(R.id.login);
-        final EditText emailEdit = view.findViewById(R.id.emailEdit);
-        final EditText passwordEdit = view.findViewById(R.id.passwordEdit);
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                HashMap<String, String> map = new HashMap<>();
-
-                map.put("email", emailEdit.getText().toString());
-                map.put("password", passwordEdit.getText().toString());
-
-                Call<LoginResult> call = retrofitInterface.executeLogin(map);
-
-                call.enqueue(new Callback<LoginResult>() {
-                    @Override
-                    public void onResponse(Call<LoginResult> call, Response<LoginResult> response) {
-
-                        if (response.code() == 200) {
-
-                            LoginResult result = response.body();
-
-                            AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
-                            builder1.setTitle(result.getName());
-                            builder1.setMessage(result.getEmail());
-
-                            builder1.show();
-
-                        } else if (response.code() == 404) {
-                            Toast.makeText(MainActivity.this, "Wrong Credentials",
-                                    Toast.LENGTH_LONG).show();
-                        }
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<LoginResult> call, Throwable t) {
-                        Toast.makeText(MainActivity.this, t.getMessage(),
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
-
-            }
-        });
-
-    }
-
-
-
-
-
-    //input
-
-    private void handleSignupDialog() {
-
-        View view = getLayoutInflater().inflate(R.layout.signup_dialog, null);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setView(view).show();
-
-        Button signupBtn = view.findViewById(R.id.signup);
-        final EditText nameEdit = view.findViewById(R.id.nameEdit);
-        final EditText emailEdit = view.findViewById(R.id.emailEdit);
-        final EditText passwordEdit = view.findViewById(R.id.passwordEdit);
-
-
-        signupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                HashMap<String, String> map = new HashMap<>();
-
-                map.put("siteID", nameEdit.getText().toString());
-                map.put("name", emailEdit.getText().toString());
-                map.put("location", passwordEdit.getText().toString());
-
-                Call<Void> call = retrofitInterface.executeSignup(map);
-
-                call.enqueue(new Callback<Void>() {
-                    @Override
-                    public void onResponse(Call<Void> call, Response<Void> response) {
-
-                        if (response.code() == 200) {
-                            Toast.makeText(MainActivity.this,
-                                    "Signed up successfully", Toast.LENGTH_LONG).show();
-                        } else if (response.code() == 400) {
-                            Toast.makeText(MainActivity.this,
-                                    "Already registered", Toast.LENGTH_LONG).show();
-                        }
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<Void> call, Throwable t) {
-                        Toast.makeText(MainActivity.this, t.getMessage(),
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
-
-            }
-        });
-
-    }
-
-
 
     // Button Action for navigating to the Registering and navigating to the pages (ADD Your Page Path from Here as TESTING DEVELOPNMENT)-->>>
     public void GoRegAct(View view) {
--- app/src/main/java/com/example/fualapp/RetrofitInterface.java
+++ app/src/main/java/com/example/fualapp/RetrofitInterface.java
@@ -9,12 +9,6 @@ import retrofit2.http.POST;
 
 public interface RetrofitInterface {
 
-    @POST("/login")
-    Call<com.example.fualapp.LoginResult> executeLogin(@Body HashMap<String, String> map);
-
-    @POST("/site/create")
-    Call<Void> executeSignup (@Body HashMap<String, String> map);
-
     @POST("/normaluser/create-R-normal-user")
     Call<Void> executeAddNewVehicle (@Body HashMap<String, String> map);
 
